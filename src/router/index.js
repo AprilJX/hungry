6e883f3e80717115f0331e2220aa6b61aeba7f7c
1040080677@qq.com <Jx19930416@>
2018-08-28 10:03:59 +0800

    饿了么页面

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Goods from 'components/goods/goods';
+import Ratings from 'components/ratings/ratings';
+import Sellers from 'components/sellers/sellers';
+
+Vue.use(Router);
+
+export default new Router({
+	linkActiveClass: 'active',
+	routes: [
+		{
+			path: '/',
+			component: Goods
+		},
+		{
+			path: '/goods',
+			name: 'goods',
+			component: Goods
+		},
+		{
+			path: '/ratings',
+			name: 'ratings',
+			component: Ratings
+		},
+		{
+			path: '/sellers',
+			name: 'sellers',
+			component: Sellers
+		}
+	]
+});
